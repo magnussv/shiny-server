3258636e8615ee996d90c352780ed72804de15d0
Magnus Svensson <magnus_mss@hotmail.com>
2016-03-25 18:36:24 +0100

    Added Anomalies detection
    Anomalies detection with conditional panel if TRUE, where alpha and max number of anomalies could be defined.

--- dogdrinkr/ui.R
+++ dogdrinkr/ui.R
@@ -10,6 +10,20 @@ shinyUI(fluidPage(
       checkboxInput("showannot", label = "Show Annotation", value = FALSE),
       checkboxInput("showgrid", label = "Show Grid", value = TRUE),
       checkboxInput("showtable", label = "Show Table", value = FALSE),
+      checkboxInput("showanoms", label = "Detect Anomalies", value = FALSE),
+      
+      # Display this only if showanoms = TRUE
+      conditionalPanel(condition = "input.showanoms == true",
+                       sliderInput(inputId = "max_anoms_adjust",
+                                   label = "Maximum number of anomalies as a percentage of the data points",
+                                   min = 1, max = 49, value = 10, step = 1, ticks = FALSE)
+      ),
+      # Display this only if showanoms = TRUE
+      conditionalPanel(condition = "input.showanoms == true",
+                       sliderInput(inputId = "alpha_adjust",
+                                   label = "Significance level, as a percentage, with which to accept or reject anomalies",
+                                   min = 1, max = 49, value = 5, step = 1, ticks = FALSE)
+      ),
       hr(),
       div(strong("From: "), textOutput("from", inline = TRUE)),
       div(strong("To: "), textOutput("to", inline = TRUE)),
