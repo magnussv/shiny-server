45ce83e1ca8fa49b1effa36abb570baa61a4450f
Magnus Svensson <magnus_mss@hotmail.com>
2016-04-17 23:09:29 +0200

    Update server.R

--- Allsvenskan/server.R
+++ Allsvenskan/server.R
@@ -99,12 +99,24 @@ output$mytable1_show_season <- renderUI({
 
 #seasons <- sort(unique(as.character(df_picker$SEASON)), decreasing = TRUE) # last season first
 
-selectizeInput(inputId = "mytable1_show_season",
+selectInput(inputId = "mytable1_show_season",
                       label = 'Välj säsong(er):',
                       choices = 2016:2001, #faster solution instead of: choices = seasons,
                       selected = 2016, #faster solution instead of: selected = max( seasons ),
-                      multiple = TRUE)#,
-                      #selectize = FALSE)
+                      multiple = TRUE,
+                      selectize = TRUE,
+                      options = I(
+                      plugins: ['remove_button'],
+    delimiter: ',',
+    persist: false,
+    create: function(input) {
+        return {
+            value: input,
+            text: input
+        }
+    }
+                      )
+                      )
 })
 
 # "Position per omgång" panel: myplot1_show_season
